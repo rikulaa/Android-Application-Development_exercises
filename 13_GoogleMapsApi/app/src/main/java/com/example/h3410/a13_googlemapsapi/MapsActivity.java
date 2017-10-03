86bebd59f2fa5624b829b8079aa492393ac28a49
RikLaa <riku.laajala@gmail.com>
2017-10-03 11:15:34 +0300

    exercise 13 started

---
+++ 13_GoogleMapsApi/app/src/main/java/com/example/h3410/a13_googlemapsapi/MapsActivity.java
@@ -0,0 +1,115 @@
+package com.example.h3410.a13_googlemapsapi;
+
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private JSONArray locations;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        FetchDataTask task = new FetchDataTask();
+        task.execute("http://student.labranet.jamk.fi/~H3410/android-application-development/res/otot/otot.json");
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void addNewMarker(JSONObject obj, GoogleMap nMap) {
+
+    }
+
+    class FetchDataTask extends AsyncTask<String, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            HttpURLConnection urlConnection = null;
+            JSONObject json = null;
+            try {
+                URL url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                json = new JSONObject(stringBuilder.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) urlConnection.disconnect();
+            }
+            return json;
+        }
+
+        protected void onPostExecute(JSONObject json) {
+            StringBuffer text = new StringBuffer("");
+            try {
+                // store highscores
+                locations = json.getJSONArray("locations");
+                for (int i=0;i < locations.length();i++) {
+                    JSONObject location = locations.getJSONObject(i);
+
+                    // Add a marker in Sydney and move the camera
+                    LatLng otto = new LatLng(Double.parseDouble(location.getString("lat")), Double.parseDouble(location.getString("lon")));
+                    mMap.addMarker(new MarkerOptions().position(otto).title(location.getString("location")));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(otto));
+                    //text.append(hs.getString("name")+":"+hs.getString("score")+"\n");
+                }
+            } catch (JSONException e) {
+                Log.e("JSON", "Error getting data.");
+            }
+            // show highscores in text view
+
+        }
+    }
+}
